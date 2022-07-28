29e3acff37ec35bff0b084f720fcc5b94ba83d98
shota <contact@shota.dev>
2022-07-28 10:27:58 +0200

    Fix error code

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])  {
     if (argc == 1) {
         printf("USAGE:\n");
         printf("    %s <TEXT>...\n", argv[0]);
-        return 0;
+        return 1;
     }
 
     int width;
