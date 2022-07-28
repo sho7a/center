a96bbc09e542fe225649aa76369fae0bc34981da
shota <contact@shota.dev>
2022-07-28 09:05:23 +0200

    Add Windows support

--- src/main.c
+++ src/main.c
@@ -4,15 +4,18 @@
 #include <Windows.h>
 #endif
 
+void printc(char *text, int width) {
+    printf("%*s", -width, printf("%*s", (width + strlen(text)) / 2, text));
+}
+
 int main(int argc, char *argv[])  {
     #ifdef _WIN32
         CONSOLE_SCREEN_BUFFER_INFO csbi;
         GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
         int width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-
         for (int i = 1; i < argc; ++i) {
-            printf("%s\n", argv[i]);
+            printc(argv[i], width);
         }
     #endif
-	return 0;
+    return 0;
 }
